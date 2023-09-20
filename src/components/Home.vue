30a1e1adeca39614276ec7fbfef1b4955327f461
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2023-09-20 23:12:59 +0100

    any sin

---
+++ src/components/Home.vue
@@ -0,0 +1,21 @@
+<template>
+        <div class="main">
+            <div class="hero">
+                    oklm
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import Home from '@/components/Home.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    Home
+  }
+}
+</script>
